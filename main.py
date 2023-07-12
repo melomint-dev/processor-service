0107ddae052dfc54a5120f0aa4b497b4a33afa64
Raj <varasaniraj5@gmail.com>
2023-07-13 00:28:50 +0530

    remove hello temp fun

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
-from typing import Union
+from dotenv import load_dotenv
 
+load_dotenv(".env")
+
+from typing import Union
 from fastapi import FastAPI
 from fastapi import File, UploadFile
+from pymongo import MongoClient
+from db import db
+import bson
 
 app = FastAPI()
 
-import embeddings 
+import embeddings
 
-# print()
 
 @app.get("/")
 def read_root():
@@ -15,17 +20,32 @@ def read_root():
 
 
 @app.post("/embeddings")
-def upload(file: UploadFile = File(...)):
+def upload(file: UploadFile = File(...), song_id: str = None):
     try:
         contents = file.file.read()
-        with open(file.filename, 'wb') as f:
+        with open("./music/" + file.filename, "wb") as f:
             f.write(contents)
-        
+
         music_embeddings = embeddings.getEmbeddings(file.filename)
 
-        return {"message": f"Successfully uploaded {file.filename}", "embeddings": music_embeddings, "filename": file.filename}
+        # Save Embeddings to DB
+        db.songs.insert_one(
+            {
+                "song_id": song_id,
+                # convert numpy array to 2d array
+                "embeddings": music_embeddings,
+            }
+        )
 
-    except Exception:
+        return {
+            "message": f"Successfully uploaded {file.filename}",
+            "embeddings": music_embeddings,
+            "filename": file.filename,
+        }
+
+    except Exception as e:
+        # Handle the exception and print its details
+        print(f"An exception occurred: {type(e).__name__} - {e}")
         return {"message": "There was an error uploading the file"}
     finally:
         file.file.close()

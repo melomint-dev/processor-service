9fca19e767353a2c899693a74374d377d415a872
Raj <varasaniraj5@gmail.com>
2023-07-16 23:33:25 +0530

    🛠️  add logs

--- main.py
+++ main.py
@@ -137,7 +137,6 @@ def upload(
     except Exception as e:
         traceback.print_exc()
         # Handle the exception and print its details
-        print(e)
         print(f"An exception occurred: {type(e).__name__} - {e}")
         return {
             "message": "There was an error uploading the file",
